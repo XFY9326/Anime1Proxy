12f070fa0e953d4f2148701024f9f72ef7f7d83b
XFY9326 <xfy9326@qq.com>
2024-10-06 16:29:15 +0800

    Update workflow

--- src/test/kotlin/tool/xfy9326/ApplicationTest.kt
+++ src/test/kotlin/tool/xfy9326/ApplicationTest.kt
@@ -1,20 +1,10 @@
 package tool.xfy9326
 
-import io.ktor.client.request.*
-import io.ktor.http.*
 import io.ktor.server.testing.*
-import tool.xfy9326.routing.configureRouting
 import kotlin.test.Test
-import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
-        application {
-            configureRouting()
-        }
-        client.get("/").apply {
-            assertEquals(HttpStatusCode.OK, status)
-        }
     }
 }
